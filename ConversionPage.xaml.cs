1d29d82d4880798b1f912149112cbdbf41a712a7
Adam Cardenas <cevaris@gmail.com>
2017-09-21 14:31:01 -0600

    working 2 swipe

--- ConversionPage.xaml.cs
+++ ConversionPage.xaml.cs
@@ -4,65 +4,25 @@ using Xamarin.Forms;
 
 namespace Conversion
 {
-    public partial class ConversionPage : ContentPage
+    public partial class ConversionPage : CarouselPage
     {
-        List<UnitType> TempUnits { get; set; }
-
-        private static ILogger logger = new ConsoleLogger(nameof(ConversionPage));
-
-        private static Conversions conversions = Conversions.Instance;
-
         public ConversionPage()
         {
             InitializeComponent();
 
-            TempUnits = Units.TemperatureOptions;
+            ItemsSource = new UnitGroup[] {
+                UnitGroup.Distance,
+                UnitGroup.Temperature
+            };
 
-            pickerLeft.ItemsSource = TempUnits;
-            pickerLeft.SelectedIndex = 0;
-            pickerRight.ItemsSource = TempUnits;
-            pickerRight.SelectedIndex = 1;
-
-            NumLeft.Text = "1";
-            recalculate();
-        }
-
-        private void recalculate()
-        {
-            UnitType typeLeft = TempUnits[pickerLeft.SelectedIndex];
-            UnitType typeRight = TempUnits[pickerRight.SelectedIndex];
-
-            Double result = conversions.Convert(UnitGroup.Temperature, typeLeft, typeRight, Convert.ToDouble(NumLeft.Text));
-            NumRight.Text = Math.Round(result, 6).ToString();
-        }
-
-        void OnChangedEvent(object sender, System.EventArgs e)
-        {
-            if (sender == SwapButton)
+            ItemTemplate = new DataTemplate(() =>
             {
-                var tmp = pickerLeft.SelectedIndex;
-                pickerLeft.SelectedIndex = pickerRight.SelectedIndex;
-                pickerRight.SelectedIndex = tmp;
-                recalculate();
-            }
+                return new UnitPage();
+            });
 
-            if(sender == pickerLeft || sender == pickerRight)
-            {
-                Picker picker = sender as Picker;
-                if (picker.SelectedIndex > 0)
-                {
-                    recalculate();
-                }
-            }
-        }
-
-        void OnTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
-        {
-            string text = ((Entry)sender).Text;
-            if (!String.IsNullOrEmpty(text))
-            {
-                recalculate();
-            }
+            //ItemsSource = new List<ContentPage>(){
+            //    new UnitPage()
+            //};
         }
     }
 }
---
+++ Views/UnitView.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Conversion
+{
+    public partial class UnitPage : ContentPage
+    {
+        List<UnitType> TempUnits { get; set; }
+
+        private static ILogger logger = new ConsoleLogger(nameof(UnitPage));
+
+        private static Conversions conversions = Conversions.Instance;
+
+        public UnitPage()
+        {
+            InitializeComponent();
+
+            TempUnits = Units.TemperatureOptions;
+
+            pickerLeft.ItemsSource = TempUnits;
+            pickerLeft.SelectedIndex = 0;
+            pickerRight.ItemsSource = TempUnits;
+            pickerRight.SelectedIndex = 1;
+
+            NumLeft.Text = "1";
+            recalculate();
+        }
+
+        private void recalculate()
+        {
+            UnitType typeLeft = TempUnits[pickerLeft.SelectedIndex];
+            UnitType typeRight = TempUnits[pickerRight.SelectedIndex];
+
+            Double result = conversions.Convert(UnitGroup.Temperature, typeLeft, typeRight, Convert.ToDouble(NumLeft.Text));
+            NumRight.Text = Math.Round(result, 6).ToString();
+        }
+
+        void OnChangedEvent(object sender, System.EventArgs e)
+        {
+            if (sender == SwapButton)
+            {
+                var tmp = pickerLeft.SelectedIndex;
+                pickerLeft.SelectedIndex = pickerRight.SelectedIndex;
+                pickerRight.SelectedIndex = tmp;
+                recalculate();
+            }
+
+            if (sender == pickerLeft || sender == pickerRight)
+            {
+                Picker picker = sender as Picker;
+                if (picker.SelectedIndex > 0)
+                {
+                    recalculate();
+                }
+            }
+        }
+
+        void OnTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+            if (!String.IsNullOrEmpty(text))
+            {
+                recalculate();
+            }
+        }
+    }
+}
